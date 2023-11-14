baf894eeea3c12d522c6478c9f854a98d688c046
Alexis Bartoloccini <bartoloccini.a@gmail.com>
2023-11-14 15:29:38 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Lógica para el análisis de imágenes (se agregará en ejercicios siguientes)
+    console.log('Análisis de imágenes');
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para la generación de imágenes (se agregará en ejercicios siguientes)
+    console.log('Generación de imágenes');
+  };
+
+  return (
+    <div>
+      <h1>Mi Aplicación de Análisis de Imágenes</h1>
+      <label htmlFor="imageUrl">URL de la Imagen:</label>
+      <input
+        type="text"
+        id="imageUrl"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <br />
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
